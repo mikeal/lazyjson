de68b6c9df1882b6b5500c75dab4714210427591
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-11-24 23:22:13 -0800

    Making index lookups and sets selective rather than iterative.

--- lazyjson.py
+++ lazyjson.py
@@ -15,6 +15,25 @@ def scan_to_next(s, index, ignore=[' ']):
     while s[index] not in ignore:
         index += 1
     return s[index], index
+
+control_map = {'{':'}','"':'"','[':']'}
+
+def scan_to_end(s, index, control=',', end_index=None):
+    control_queue = []
+    index -= 1
+    x = 'fghjhvcvbnmmnbvcvbnm'
+    while x != control and len(control_queue) is not 0:
+        index += 1
+        if end_index is not None and index > end_index:
+            e = IndexError("Reached end_index without finding control character.")
+            e.index = index
+            raise e
+        x = s[index]
+        if x in control_map:
+            control_queue.append()
+        elif len(control_queue) is not 0 and x == control_map[control_queue[-1]]:
+            control_queue.pop()
+    return index
     
 def find_not_escaped(string, sub, index, escape='\\'):
     index = string.find(sub, index)
@@ -30,6 +49,7 @@ class Parser(object):
         self.readsize = readsize
         self.buffer = ""
         self.decoded_chunks = {}
+        self.read_chunk()
     def read_chunk(self):
         block = self.stream.read(self.readsize)
         self.buffer += block
@@ -47,43 +67,63 @@ class LazyList(list):
         self.current_index = start_index
         self.end_index = end_index
         self.additional_items = []
-        self.index_buffer_map = {}
-    def _find_next_item(self):
-        if self.parser.buffer[self.current_index] == ']':
-            raise IndexError("Already read to end of array")
-        # Attempt to insure , or ] in the current block
-        while ( find_not_escaped(self.parser.buffer, ',', self.current_index) is -1 ) and ( 
-                find_not_escaped(self.parser.buffer, ']', self.current_index) is -1 ):
-            if not self.parser.read_chunk():
-                raise Exception("FAIL!")
-        s, start_index = scan_to_next(self.parser.buffer, self.current_index)
-        end_index = find_not_escaped(self.parser.buffer, s, start_index + 1)
+        self.unparsed_length = None
+        self.index_buffer_map = {} # {0:(56,190,)}
+        self.internal_index_map = {} # {0:'example'}
         
+    def _find_item(self, index):
+        if self.end_index is None:
+            self._set_end_index()
+        
+        i = self.start_index
+        for x in range(index):
+            i = scan_to_end(self.parser.buffer, i, ',', self.end_index)
+        
+        s, start_index = scan_to_next(self.parser.buffer, i)
+        end_index = scan_to_end(self.parser.buffer, start_index, 
+                                control_map[self.parser.buffer[start_index]])
+                                
         obj = type_map[s](self.parser,start_index,end_index) 
-        self.parser[(start_index, end_index,)] = obj
+        self.index_buffer_map[index] = (start_index, end_index,)
         return obj
-        
-    def _set_next(self):
-        obj, start, end = self._find_next_item()
-        list.append(self, obj)
-        self.index_buffer_map[list.__len__(self) - 1] = (start, end,)
-            
+                
     def __getitem__(self, i):
-        if list.__len__(self) >= (i + 1):
-            return list.__getitem__(self, i)
-        else:
-            while list.__len__(self) < (i + 1): 
-                self._set_next()
-            return list.__getitem__(self, i)
+        if i in self.internal_index_map:
+            return self.internal_index_map[i]
+        value = self._find_item(i)
+        self.internal_index_map[i] = value
+        return value
     
-    def __setitem__(self, i, value):    
-        while list.__len__(self) < (i + 1): 
-            self._set_next()
-        list.__setitem__(self, i, value)
+    def __setitem__(self, i, value):
+        self[i]
+        self.internal_index_map[i] = value
         self.parser.decoded_chunks[self.index_buffer_map[i]] = value
     
+    def _set_end_index(self):
+        i = self.start_index
+        while self.end_index is None:
+            try:
+                self.end_index = scan_to_end(self.parser.buffer, i, ']')
+            except IndexError, e:
+                self.parser.buffer.read_chunk()
+                i = e.index
+        
+    def __len__(self):
+        if self.unparsed_length is None:
+            if self.end_index is None:
+                self._set_end_index()
+            working = True
+            i = self.start_index
+            self.unparsed_length = 0
+            while working:
+                try: 
+                    scan_to_end(self.parser.buffer, i, ',')
+                    self.unparsed_length += 1
+                except IndexError, e: 
+                    working = False
+        return self.unparsed_length + len(self.additional_items)
+    
     def append(self, value):
-        list.append(self, value)
         self.additional_items.append(value)
         
 
